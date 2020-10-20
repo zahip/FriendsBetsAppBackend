d41c978052841a90e92b61084ae83a5aeb0ac9b9
Zahi Peri <zahip8@outlook.com>
2020-10-20 18:00:32 +0300

    add nodemon and cors

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 const express = require('express')
-const app = express()
-const port = 8000
+const cors = require('cors')
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+
+// app config
+const app = express()
+const port = process.env.PORT || 8000
+
+// middlewares
+app.use(cors())
+app.use(express.json())
+
+// db config
+
+// api routes
 
 app.get('/', (req, res) => {
     console.log('hello api')
-  res.send("zahi")
+  res.status(200).send("zahi")
 })
 
+//listen
+
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`app listening at http://localhost:${port}`)
 })
